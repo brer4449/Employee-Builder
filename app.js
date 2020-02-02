cd1ab7410523fe3301e2a7ece33eda1717a8d59c
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-02 11:15:09 -0500

    Questions now chain (no exit yet)

--- app.js
+++ app.js
@@ -30,84 +30,90 @@ const questions = [
   }
 ];
 
-inquirer.prompt(questions).then(function(answers) {
-  switch (answers.title) {
-    case "Manager":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is your office number?",
-            name: "officeNum"
-          }
-        ])
-        .then(function(managerAnswer) {
-          let newManager = createManager(answers, managerAnswer);
-          let writeData = [
-            `Name: ${newManager.getName()}`,
-            `ID: ${newManager.getId()}`,
-            `Email: ${newManager.getEmail()}`,
-            `Role: ${newManager.getRole()}`,
-            `Office Number: ${newManager.officeNum}`,
-            "-".repeat(20)
-          ].join("\n");
-          fs.appendFile("log.txt", writeData, err => {
-            if (err) throw err;
+questionPrompt = () => {
+  inquirer.prompt(questions).then(function(answers) {
+    switch (answers.title) {
+      case "Manager":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is their office number?",
+              name: "officeNum"
+            }
+          ])
+          .then(function(managerAnswer) {
+            let newManager = createManager(answers, managerAnswer);
+            let writeData = [
+              `Name: ${newManager.getName()}`,
+              `ID: ${newManager.getId()}`,
+              `Email: ${newManager.getEmail()}`,
+              `Role: ${newManager.getRole()}`,
+              `Office Number: ${newManager.officeNum}`,
+              "-".repeat(20)
+            ].join("\n");
+            fs.appendFile("log.txt", writeData, err => {
+              if (err) throw err;
+            });
+            questionPrompt();
           });
-        });
-      break;
-    case "Engineer":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is your Github user name?",
-            name: "github"
-          }
-        ])
-        .then(function(engineerAnswer) {
-          let newEngineer = createEngineer(answers, engineerAnswer);
-          let writeData = [
-            `Name: ${newEngineer.getName()}`,
-            `ID: ${newEngineer.getId()}`,
-            `Email: ${newEngineer.getEmail()}`,
-            `Role: ${newEngineer.getRole()}`,
-            `Github Username: ${newEngineer.getGithub()}`,
-            "-".repeat(20)
-          ].join("\n");
-          fs.appendFile("log.txt", writeData, err => {
-            if (err) throw err;
+        break;
+      case "Engineer":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is their Github user name?",
+              name: "github"
+            }
+          ])
+          .then(function(engineerAnswer) {
+            let newEngineer = createEngineer(answers, engineerAnswer);
+            let writeData = [
+              `Name: ${newEngineer.getName()}`,
+              `ID: ${newEngineer.getId()}`,
+              `Email: ${newEngineer.getEmail()}`,
+              `Role: ${newEngineer.getRole()}`,
+              `Github Username: ${newEngineer.getGithub()}`,
+              "-".repeat(20)
+            ].join("\n");
+            fs.appendFile("log.txt", writeData, err => {
+              if (err) throw err;
+            });
+            questionPrompt();
           });
-        });
-      break;
-    case "Intern":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What school did you go to?",
-            name: "school"
-          }
-        ])
-        .then(function(internAnswer) {
-          let newIntern = createIntern(answers, internAnswer);
-          let writeData = [
-            `Name: ${newIntern.getName()}`,
-            `ID: ${newIntern.getId()}`,
-            `Email: ${newIntern.getEmail()}`,
-            `Role: ${newIntern.getRole()}`,
-            `Alma Mater: ${newIntern.getSchool()}`,
-            "-".repeat(20)
-          ].join("\n");
-          fs.appendFile("log.txt", writeData, err => {
-            if (err) throw err;
+        break;
+      case "Intern":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What school did they go to?",
+              name: "school"
+            }
+          ])
+          .then(function(internAnswer) {
+            let newIntern = createIntern(answers, internAnswer);
+            let writeData = [
+              `Name: ${newIntern.getName()}`,
+              `ID: ${newIntern.getId()}`,
+              `Email: ${newIntern.getEmail()}`,
+              `Role: ${newIntern.getRole()}`,
+              `Alma Mater: ${newIntern.getSchool()}`,
+              "-".repeat(20)
+            ].join("\n");
+            fs.appendFile("log.txt", writeData, err => {
+              if (err) throw err;
+            });
+            questionPrompt();
           });
-        });
-      break;
-    default:
-      console.log("You must choose!");
-  }
-});
+        break;
+      default:
+        console.log("You must choose!");
+    }
+  });
+};
+questionPrompt();
 
 //Functions to get user input and then return it to CLI
 let createManager = (answers, managerAnswer) => {
